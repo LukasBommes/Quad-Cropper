808b4bf61d1b7f9793f5a3cd0c7ef790cef10cd8
LukasBommes <lukas.bommes@gmx.de>
2022-09-21 15:51:43 +0200

    initial progress

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import sys
 import os
 import glob
+import json
 import cv2
 
 from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog
-from PySide6.QtCore import Qt, QFile, Slot
-from PySide6.QtGui import QPixmap, QImage
+from PySide6.QtCore import Qt, QObject, Slot, Signal, QPointF
+from PySide6.QtGui import QPixmap, QImage, QPainter
 from src.ui_mainwindow import Ui_MainWindow
+
+
+#TODO:
+# click "new rectangle"
+# click four corner points, on each click draw a point
+# once we have four points, draw a rectangle, add rectangale to list of rectangles for this image
 
 
 class MainWindow(QMainWindow):
@@ -15,7 +22,11 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.imageLabel.setGeometry(0, 0, 640, 512)  # initial size
-        self.current_image = None
+        self.model = Model()
+
+        self.model.current_image_changed.connect(lambda image: self.update_image_label(image))
+        self.model.current_rectangle_changed.connect(self.current_rectangle_changed)
+        self.model.rectangles_changed.connect(self.rectangles_changed)
 
         # menu actions
         self.ui.actionOpen_Folder.triggered.connect(self.open_folder)
@@ -23,14 +34,21 @@ class MainWindow(QMainWindow):
         self.ui.actionCrop_All.triggered.connect(self.crop_all)
         
         # buttons
+        self.ui.newRectangleButton.clicked.connect(self.new_rectangle_button_clicked)
+        self.ui.deleteRectangleButton.clicked.connect(self.delete_rectangle_button_clicked)
 
 
         # image selection changed
         self.ui.imagesListWidget.currentItemChanged.connect(self.image_selection_changed)
 
 
+    def reset(self):
+        self.model.current_rectangle = []
+        self.model.current_image = None
+
+
     def resizeEvent(self, event):
-        self.update_image_label()
+        self.update_image_label(self.model.current_image)
 
 
     def open_folder(self):
@@ -39,8 +57,14 @@ class MainWindow(QMainWindow):
         if dir == "":
             return
         self.image_dir = dir
+        # check if meta.json is available and load into model
+        try:
+            with open(os.path.join(dir, "meta.json"), "r") as file:
+                self.model.rectangles = json.load(file)
+        except FileNotFoundError:
+            pass
         self.load_image_files()
-        self.populate_image_list()
+        self.populate_image_list()      
 
 
     def close_folder(self):
@@ -68,7 +92,8 @@ class MainWindow(QMainWindow):
 
 
     @Slot()
-    def image_selection_changed(self): #, current, previous):
+    def image_selection_changed(self):
+        self.reset()
         current = self.ui.imagesListWidget.currentItem()
         if not current:
             return
@@ -79,46 +104,207 @@ class MainWindow(QMainWindow):
 
     def load_selected_image(self, selected_image_file):
         image_file = os.path.join(self.image_dir, selected_image_file)
+        
+        # TODO: check in model.rectangles if rectangles exist for this image...
+        #self.update_image_label()
+
         image = cv2.imread(image_file)
         height, width, _ = image.shape[:3]
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
         bytesPerLine = 3 * width
         qt_image = QImage(image.data, width, height, bytesPerLine, QImage.Format_RGB888)
-        self.current_image = QPixmap(qt_image)
-        self.update_image_label()
+        self.model.current_image = {
+            "filename": selected_image_file,
+            "pixmap": QPixmap(qt_image)
+        }
 
 
     @Slot()
-    def update_image_label(self):
-        if self.current_image:
+    def update_image_label(self, image):
+        if image: #self.model.current_image:
             w = self.ui.imageLabel.width()
             h = self.ui.imageLabel.height()
-            self.ui.imageLabel.setPixmap(self.current_image.scaled(w, h, Qt.KeepAspectRatio))
+            self.ui.imageLabel.setPixmap(image["pixmap"].scaled(w, h, Qt.KeepAspectRatio))
             self.ui.imageLabel.mousePressEvent = self.getImagePos
 
     
     def getImagePos(self, event):
-        # get current scale factor
-        width_scaled = self.ui.imageLabel.width()
-        height_scaled = self.ui.imageLabel.height()
-        width_orig = self.current_image.size().width()
-        height_orig = self.current_image.size().height()
-
+        # get current image scale
+        width_scaled = self.ui.imageLabel.pixmap().size().width() #self.ui.imageLabel.width()
+        height_scaled = self.ui.imageLabel.pixmap().size().height() #self.ui.imageLabel.height()
+        width_orig = self.model.current_image["pixmap"].size().width()
+        height_orig = self.model.current_image["pixmap"].size().height()
         width_scale = width_orig / width_scaled
         height_scale = height_orig / height_scaled
 
-        x = width_scale * event.position().x()
-        y = height_scale * event.position().y()
-        print(x, y)
-        #c = self.img.pixel(x,y)  # color code (integer): 3235912
-        # depending on what kind of value you like (arbitary examples)
-        #c_qobj = QColor(c)  # color object
-        #c_rgb = QColor(c).getRgb()  # 8bit RGBA: (255, 23, 0, 255)
-        #c_rgbf = QColor(c).getRgbf()  # RGBA float: (1.0, 0.3123, 0.0, 1.0)
-        #return x, y, c_rgb
+        x_scaled = event.position().x()
+        y_scaled = event.position().y()
+        x_orig = width_scale * x_scaled
+        y_orig = height_scale * y_scaled
+        print(x_scaled, y_scaled, x_orig, y_orig)
+        self.add_point_to_current_rectangle(x_orig, y_orig)
 
+        # # TODO: load from json and draw based on model data
+        # pixmap = self.ui.imageLabel.pixmap().copy()
+        # #pixmap = self.model.current_image["pixmap"].copy()
+        # painter = QPainter(pixmap)
+        # painter.setBrush(Qt.red)
+        # painter.drawEllipse(event.position(), 5, 5)
+        # painter.end()
+        # #self.ui.imageLabel.setPixmap(pixmap)
+        # self.model.current_image["pixmap"] = pixmap
+
+
+    def add_point_to_current_rectangle(self, x, y):
+        current_rectangle_copy = self.model.current_rectangle.copy()
+        current_rectangle_copy.append((x, y))
+        print("self.model.current_rectangle: ", current_rectangle_copy)
+        self.model.current_rectangle = current_rectangle_copy
+        if len(current_rectangle_copy) < 4:
+            return
+        self.create_new_rectangle()
         
+
+    def create_new_rectangle(self):
+        image_file = self.model.current_image["filename"]
+        rectangles_copy = self.model.rectangles.copy()
+        try:
+            rectangles_copy[image_file].append(
+                self.model.current_rectangle
+            )
+        except KeyError:
+            rectangles_copy[image_file] = [
+                self.model.current_rectangle
+            ]
+        self.model.rectangles = rectangles_copy
+        self.model.current_rectangle = []
+        print("self.model.current_rectangle: ", self.model.current_rectangle)
+        print("self.model.rectangles: ", self.model.rectangles)
+
+
+    def draw_rectangles(self):
+        if not self.model.current_image:
+            return
+
+        #file_name = self.current_image["filename"]
+        #for rectangle in self.model.rectangles[file_name]:
+
+        pixmap = self.ui.imageLabel.pixmap().copy()
+        #pixmap = self.model.current_image["pixmap"].copy()
+        painter = QPainter(pixmap)
+        painter.setBrush(Qt.red)
+
+        image_file = self.model.current_image["filename"]
+        for rectangle in self.model.rectangles[image_file]:
+            for corner in rectangle:
+                painter.drawEllipse(QPointF(*corner), 5, 5)
+
+        painter.end()
+        self.ui.imageLabel.setPixmap(pixmap)
+        #self.model.current_image["pixmap"] = pixmap
+
+
+    @Slot()
+    def current_rectangle_changed(self):
+        if not self.model.current_image:
+            return
+
+        print("current rectangle changed")
+        pixmap = self.ui.imageLabel.pixmap().copy()
+        #pixmap = self.model.current_image["pixmap"].copy()
+        painter = QPainter(pixmap)
+        painter.setBrush(Qt.red)
+        for corner in self.model.current_rectangle:
+            painter.drawEllipse(QPointF(*corner), 5, 5)
+        painter.end()
+        self.ui.imageLabel.setPixmap(pixmap)
+
+    
+    @Slot()
+    def rectangles_changed(self):
+        print("rectangles changed")
+        # TODO: comment back in later...
+        #with open(os.path.join(self.image_dir, "meta.json"), "w") as file:
+        #    json.dump(self.model.rectangles, file)
+        #self.populate_rectangle_list()
+        self.draw_rectangles()
+
+
+    # TODO: finish this function, highlight selected rectangle
+    #def populate_rectangle_list(self):
+    #    file_name = self.current_image["filename"]
+    #    try:
+    #        rectangle = self.model.rectangles[file_name]
+    #        self.ui.rectanglesListWidget.addItem(file_name)
+    #    except KeyError:
+    #        pass
+            
+
+    #def draw_current_rectangle(self):
+    #    if len(self.model.current_rectangle) == 4:
+    #        # draw corners + rectangle
+    #        pass
+    #    else:
+    #        # draw corners
+    #        for corner in self.model.current_rectangle:
+    #            print(corner)
+            
+
+    @Slot()
+    def new_rectangle_button_clicked(self):
+        if not self.model.current_image:
+            return
+        # now click four corner points (TODO. show helper text in status bar...)
+        self.model.current_rectangle = []
+
+
+    @Slot()
+    def delete_rectangle_button_clicked(self):
+        # TODO: get selected rectangle from list
+        # delete rectangle from mode.rectangles
+        # redraw
+        pass
+
+
+
+class Model(QObject):
+    rectangles_changed = Signal()
+    current_rectangle_changed = Signal(object)
+    current_image_changed = Signal(object)
+
+    def __init__(self):
+        super().__init__()
+        self._rectangles = {}
+        self._current_rectangle = []
+        self._current_image = None
+
+    @property
+    def rectangles(self):
+        return self._rectangles
+
+    @rectangles.setter
+    def rectangles(self, value):
+        self._rectangles = value
+        self.rectangles_changed.emit()
+
+    @property
+    def current_rectangle(self):
+        return self._current_rectangle
+
+    @current_rectangle.setter
+    def current_rectangle(self, value):
+        self._current_rectangle = value
+        self.current_rectangle_changed.emit(value)
+
+    @property
+    def current_image(self):
+        return self._current_image
+
+    @current_image.setter
+    def current_image(self, value):
+        self._current_image = value
+        self.current_image_changed.emit(value)
 
 
 
