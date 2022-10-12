5e9f16e33c863485722cb57d98aaeaa17028862c
LukasBommes <lukas.bommes@gmx.de>
2022-10-12 16:26:24 +0200

    moved image viewer into separate file

--- src/__main__.py
+++ src/__main__.py
@@ -9,81 +9,12 @@ def main():
     import numpy as np
     import cv2
 
-    from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox, \
-        QGraphicsView, QGraphicsScene, QGraphicsPixmapItem, QFrame
-    from PySide6.QtCore import Qt, QObject, Slot, Signal, QSettings, QPoint, QPointF, QRectF
-    from PySide6.QtGui import QPixmap, QImage, QPainter, QBrush, QColor, QPolygonF
+    from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox
+    from PySide6.QtCore import Qt, QObject, Slot, Signal, QSettings, QPointF
+    from PySide6.QtGui import QPixmap, QImage, QPainter, QColor, QPolygonF
     from src.ui_mainwindow import Ui_MainWindow
+    from src.viewer import ImageViewer
     from src.utils import sort_cw, crop_module
-
-
-    class ImageViewer(QGraphicsView):
-        imageClicked = Signal(QPoint)
-
-        def __init__(self, parent):
-            super(ImageViewer, self).__init__(parent)
-            self._zoom = 0
-            self._empty = True
-            self._scene = QGraphicsScene(self)
-            self._image = QGraphicsPixmapItem()
-            self._scene.addItem(self._image)
-            self.setScene(self._scene)
-            self.setDragMode(QGraphicsView.NoDrag)
-            self.setTransformationAnchor(QGraphicsView.AnchorUnderMouse)
-            self.setResizeAnchor(QGraphicsView.AnchorUnderMouse)
-            self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-            self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-            self.setBackgroundBrush(QBrush(QColor(30, 30, 30)))
-            self.setFrameShape(QFrame.NoFrame)
-
-        def hasImage(self):
-            return not self._empty
-
-        def fitInView(self, scale=True):
-            rect = QRectF(self._image.pixmap().rect())
-            if not rect.isNull():
-                self.setSceneRect(rect)
-                if self.hasImage():
-                    unity = self.transform().mapRect(QRectF(0, 0, 1, 1))
-                    self.scale(1 / unity.width(), 1 / unity.height())
-                    viewrect = self.viewport().rect()
-                    scenerect = self.transform().mapRect(rect)
-                    factor = min(viewrect.width() / scenerect.width(),
-                                viewrect.height() / scenerect.height())
-                    self.scale(factor, factor)
-                self._zoom = 0
-
-        def setImage(self, pixmap=None, fit_in_view=True):
-            if fit_in_view:
-                self._zoom = 0
-            if pixmap and not pixmap.isNull():
-                self._empty = False
-                self._image.setPixmap(pixmap)
-            else:
-                self._empty = True
-                self._image.setPixmap(QPixmap())
-            if fit_in_view:
-                self.fitInView()
-
-        def wheelEvent(self, event):
-            if self.hasImage():
-                if event.angleDelta().y() > 0:
-                    factor = 1.25
-                    self._zoom += 1
-                else:
-                    factor = 0.8
-                    self._zoom -= 1
-                if self._zoom > 0:
-                    self.scale(factor, factor)
-                elif self._zoom == 0:
-                    self.fitInView()
-                else:
-                    self._zoom = 0
-
-        def mousePressEvent(self, event):
-            if self._image.isUnderMouse():
-                self.imageClicked.emit(self.mapToScene(event.pos()).toPoint())
-            super(ImageViewer, self).mousePressEvent(event)
 
 
 
---
+++ src/viewer.py
@@ -0,0 +1,73 @@
+from PySide6.QtWidgets import QGraphicsView, QGraphicsScene, QGraphicsPixmapItem, QFrame
+from PySide6.QtCore import Qt, Signal, QPoint, QRectF
+from PySide6.QtGui import QPixmap, QBrush, QColor
+
+
+
+class ImageViewer(QGraphicsView):
+    imageClicked = Signal(QPoint)
+
+    def __init__(self, parent):
+        super(ImageViewer, self).__init__(parent)
+        self._zoom = 0
+        self._empty = True
+        self._scene = QGraphicsScene(self)
+        self._image = QGraphicsPixmapItem()
+        self._scene.addItem(self._image)
+        self.setScene(self._scene)
+        self.setDragMode(QGraphicsView.NoDrag)
+        self.setTransformationAnchor(QGraphicsView.AnchorUnderMouse)
+        self.setResizeAnchor(QGraphicsView.AnchorUnderMouse)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setBackgroundBrush(QBrush(QColor(30, 30, 30)))
+        self.setFrameShape(QFrame.NoFrame)
+
+    def hasImage(self):
+        return not self._empty
+
+    def fitInView(self, scale=True):
+        rect = QRectF(self._image.pixmap().rect())
+        if not rect.isNull():
+            self.setSceneRect(rect)
+            if self.hasImage():
+                unity = self.transform().mapRect(QRectF(0, 0, 1, 1))
+                self.scale(1 / unity.width(), 1 / unity.height())
+                viewrect = self.viewport().rect()
+                scenerect = self.transform().mapRect(rect)
+                factor = min(viewrect.width() / scenerect.width(),
+                            viewrect.height() / scenerect.height())
+                self.scale(factor, factor)
+            self._zoom = 0
+
+    def setImage(self, pixmap=None, fit_in_view=True):
+        if fit_in_view:
+            self._zoom = 0
+        if pixmap and not pixmap.isNull():
+            self._empty = False
+            self._image.setPixmap(pixmap)
+        else:
+            self._empty = True
+            self._image.setPixmap(QPixmap())
+        if fit_in_view:
+            self.fitInView()
+
+    def wheelEvent(self, event):
+        if self.hasImage():
+            if event.angleDelta().y() > 0:
+                factor = 1.25
+                self._zoom += 1
+            else:
+                factor = 0.8
+                self._zoom -= 1
+            if self._zoom > 0:
+                self.scale(factor, factor)
+            elif self._zoom == 0:
+                self.fitInView()
+            else:
+                self._zoom = 0
+
+    def mousePressEvent(self, event):
+        if self._image.isUnderMouse():
+            self.imageClicked.emit(self.mapToScene(event.pos()).toPoint())
+        super(ImageViewer, self).mousePressEvent(event)
